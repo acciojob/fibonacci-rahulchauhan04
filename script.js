14dcc4e92d044d7e8980751211f2c6a1cb227c5c
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-02-16 23:53:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function fibonacci(num) {
-if (num <= 0) {
-	return 0;
-} else if (num === 1) {
-	reutrn 1;
-} 
-	let a = 0, b =1 , fib = 1;
-	for (let i = 2; i <= num;  i++) {
-		fib = a +b ;
-		a = b;
-		b = fib;
-	}
-	return fib;
+    if (num <= 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    }
+
+    let a = 0, b = 1, fib = 1;
+    for (let i = 2; i < num; i++) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+    return fib;
 }
 
 module.exports = fibonacci;
