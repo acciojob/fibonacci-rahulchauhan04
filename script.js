b84fde40af0b5c233c29b36493628681ed4ae5bb
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-02-16 23:58:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function fibonacci(num) {
-    if (num <= 0) {
+function fibonacci(n) {
+    if (n <= 0) {
         return 0;
-    } else if (num === 1) {
-        return 1;
+    } else if (n === 1) {
+        return 0; // The first number in Fibonacci sequence is 0
+    } else if (n === 2) {
+        return 1; // The second number in Fibonacci sequence is 1
     }
 
     let a = 0, b = 1, fib = 1;
-    for (let i = 2; i < num; i++) {
+    for (let i = 2; i < n; i++) {
         fib = a + b;
         a = b;
         b = fib;
     }
     return fib;
 }
-
-module.exports = fibonacci;
